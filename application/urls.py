75be15aded20736278360a157060558673387993
Verified-p <verifieddeveloper13@gmail.com>
2025-06-07 13:52:28 +0300

    Update urls.py

--- application/urls.py
+++ application/urls.py
@@ -21,7 +21,7 @@ from application import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('portfolio/', views.portfolio, name='portfolio'),
